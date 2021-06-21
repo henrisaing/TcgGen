f41b5a50395a5a7d3a7dda6153684307929e8ee1
henrisaing <henri.saing@gmail.com>
2021-06-21 14:48:39 -0400

    Border style tweaks in card.css

--- tcggen/resources/views/collections/show.blade.php
+++ tcggen/resources/views/collections/show.blade.php
@@ -6,27 +6,25 @@
   <?=$collection->name?>
 </nav>
 
-<div class="container">
-  <?php print($collection->name) ?>
-  <?php print($collection->image) ?>
-  <?php print($collection->description) ?>
-  <?php print($collection->public) ?>
-  <?php if ($auth['owner']): ?>
-  <br>
-    <button class="lb-link" func="/collection/<?=$collection->id?>/set/new">new set</button>
-  <?php endif ?>
-  <?php foreach ($sets as $set): ?>
-    <?php if ($auth['owner'] || $set['public'] == 'public'): ?>
-      <br>
-      <a href="/collection/<?=$collection->id?>/set/<?=$set->id?>"><?=$set->name?></a>|
-      <?=$set->image?>|
-      <?=$set->description?>|
-      <?=$set->public?>|
-      <?php if ($auth['owner']): ?>
-        <button class="lb-link" func="/set/<?=$set->id?>/edit">edit</button>
-        <button class="lb-link" func="/set/<?=$set->id?>/delete">delete</button>
-      <?php endif ?>
-    <?php endif; ?>
-  <?php endforeach; ?>
-</div>
+<?php print($collection->name) ?>
+<?php print($collection->image) ?>
+<?php print($collection->description) ?>
+<?php print($collection->public) ?>
+<?php if ($auth['owner']): ?>
+<br>
+  <button class="lb-link" func="/collection/<?=$collection->id?>/set/new">new set</button>
+<?php endif ?>
+<?php foreach ($sets as $set): ?>
+  <?php if ($auth['owner'] || $set['public'] == 'public'): ?>
+    <br>
+    <a href="/collection/<?=$collection->id?>/set/<?=$set->id?>"><?=$set->name?></a>|
+    <?=$set->image?>|
+    <?=$set->description?>|
+    <?=$set->public?>|
+    <?php if ($auth['owner']): ?>
+      <button class="lb-link" func="/set/<?=$set->id?>/edit">edit</button>
+      <button class="lb-link" func="/set/<?=$set->id?>/delete">delete</button>
+    <?php endif ?>
+  <?php endif; ?>
+<?php endforeach; ?>
 @stop
