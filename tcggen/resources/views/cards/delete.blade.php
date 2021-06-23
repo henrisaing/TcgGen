669361509954711c9dafd144cb1f9c1be84a689a
henrisaing <henri.saing@gmail.com>
2021-06-23 12:19:59 -0400

    Adjusts lb-close button styling.

--- tcggen/resources/views/cards/delete.blade.php
+++ tcggen/resources/views/cards/delete.blade.php
@@ -3,9 +3,7 @@
   <form action="/card/<?=$card->id?>/delete" method="post">
     {{csrf_field()}}
     {{method_field('delete')}}
-    <p>
     Are you sure you want to delete [<?=$card->name?>]?
-    </p>
     <br><br>
     <button type="submit">DELETE</button>
   </form>
